14563a31cce150c61d3572a2014a063656f6c051
Tejas Raskar <tjsrskr@gmail.com>
2024-12-23 23:16:55 +0530

    Added zod validation

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { verify } from 'hono/jwt'
+import { createBlogInput, updateBlogInput } from "@traskar/bllog-common";
 
 export const blogRouter = new Hono<{
     Bindings: {
@@ -16,20 +17,34 @@ export const blogRouter = new Hono<{
 blogRouter.use("/*", async (c, next) => {
     const authHeader = c.req.header("Authorization") || " ";
     const token = authHeader.split(' ')[1];
-    const user = await verify(token, c.env.JWT_SECRET);
-    if(user) {
-        c.set("userId", String(user.id));
-        await next();
-    } else {
+    try {
+        const user = await verify(token, c.env.JWT_SECRET);
+        if(user) {
+            c.set("userId", String(user.id));
+            await next();
+        } else {
+            c.status(403);
+            return c.json({
+                message: "You are not logged in!"
+            })
+        }
+    } catch (e) {
         c.status(403);
-        c.json({
-            message: "You are not logged in!"
+        return c.json({
+                message: "You are not logged in!"
         })
     }
 })
 
 blogRouter.post("/", async (c) => {
     const body = await c.req.json();
+    const { success } = createBlogInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+        message: "Incorrect inputs"
+        })
+    }
     const authorId = c.get("userId");
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL
@@ -55,6 +70,13 @@ blogRouter.post("/", async (c) => {
 
 blogRouter.put("/", async (c) => {
     const body = await c.req.json();
+    const { success } = updateBlogInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+        message: "Incorrect inputs"
+        })
+    }
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL
     }).$extends(withAccelerate());
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign } from 'hono/jwt'
+import { signinInput, signupInput } from "@traskar/bllog-common";
 
 export const userRouter = new Hono<{
     Bindings: {
@@ -13,6 +14,13 @@ export const userRouter = new Hono<{
 
 userRouter.post("/signup", async (c) => {
     const body = await c.req.json();
+    const { success } = signupInput.safeParse(body);
+    if (!success) {
+      c.status(411);
+      return c.json({
+        message: "Incorrect inputs"
+      })
+    }
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL
     }).$extends(withAccelerate());
@@ -42,6 +50,13 @@ userRouter.post("/signup", async (c) => {
   
   userRouter.post("/signin", async (c) => {
     const body = await c.req.json();
+    const { success } = signinInput.safeParse(body);
+    if (!success) {
+      c.status(411);
+      return c.json({
+        message: "Incorrect inputs"
+      })
+    }
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL
     }).$extends(withAccelerate());
