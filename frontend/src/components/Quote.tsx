90e7e0b66ffb9a0ad083ff9692353d99a99f85db
Tejas Raskar <tjsrskr@gmail.com>
2024-12-24 17:52:46 +0530

    added quote component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,17 @@
+export const Quote = () => {
+    return <div className="bg-gray-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+                <div className="max-w-lg">
+                    <div className="text-2xl font-bold">
+                        "The customer service I received was exceptional. The support team went above and beyond to address my concerns."
+                    </div>
+                    <div className="mt-4 font-bold">
+                        Jules Winnfield
+                    </div>
+                    <div className="font-extralight">
+                        CEO, Acme Inc.
+                    </div>
+                </div>
+        </div>
+    </div>
+}
