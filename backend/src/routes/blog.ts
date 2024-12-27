97fca06dc9fd21034e1f6ea2ae51e994e0d8a24b
Tejas Raskar <tjsrskr@gmail.com>
2024-12-27 23:06:15 +0530

    modified get blog details res

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -127,6 +127,16 @@ blogRouter.get("/:id", async (c) => {
         const blog = await prisma.blog.findFirst({
             where: {
                 id: id
+            },
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                author: {
+                    select: {
+                        name: true
+                    }
+                }
             }
         });
 
