c04751caa060ccdbacaef14eaf02268e9d4dcc97
Tejas Raskar <tjsrskr@gmail.com>
2024-12-29 23:58:32 +0530

    fixed broken publish button in frontend

--- frontend/src/components/TipTap.tsx
+++ frontend/src/components/TipTap.tsx
@@ -1,56 +1,7 @@
 // src/Tiptap.tsx
-import { useEditor, EditorContent, BubbleMenu } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+import { EditorContent, BubbleMenu, Editor } from '@tiptap/react'
 
-const content = `
-<h2>
-  Hi there,
-</h2>
-<p>
-  this is a <em>basic</em> example of <strong>Bllog editor</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-</p>
-<ul>
-  <li>
-    That’s a bullet list with one …
-  </li>
-  <li>
-    … or two list items.
-  </li>
-</ul>
-<p>
-  Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-</p>
-<pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-<p>
-  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-</p>
-<blockquote>
-  Wow, that’s amazing. Good work, boy! 👏
-  <br />
-  — Mom
-</blockquote>
-`
-
-export const Tiptap = () => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit.configure({
-        heading: {
-          levels: [1, 2, 3],
-        }
-      })
-    ],
-    content,
-    editorProps: {
-      attributes: {
-        class: 'prose prose-sm sm:prose-base lg:prose-lg m-5 focus:outline-none'
-      }
-    }
-
-  })
-
+export const Tiptap = ({ editor }: { editor: Editor }) => {
   if (!editor) {
     return null
   }
--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -1,12 +1,76 @@
+import { useEditor } from "@tiptap/react"
 import { AppBar } from "../components/AppBar"
 import { Tiptap } from "../components/TipTap"
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+import StarterKit from '@tiptap/starter-kit'
+
+const content = `
+<h2>
+  Hi there,
+</h2>
+<p>
+  this is a <em>basic</em> example of <strong>Bllog editor</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+</p>
+<ul>
+  <li>
+    That’s a bullet list with one …
+  </li>
+  <li>
+    … or two list items.
+  </li>
+</ul>
+<p>
+  Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+</p>
+<pre><code class="language-css">body {
+  display: none;
+}</code></pre>
+<p>
+  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+</p>
+<blockquote>
+  Wow, that’s amazing. Good work, boy! 👏
+  <br />
+  — Mom
+</blockquote>
+`
+
 
 export const Editor = () => {
+    const editor = useEditor({
+        extensions: [
+            StarterKit.configure({
+                heading: {
+                    levels: [1, 2, 3],
+                }
+            })
+        ],
+        content,
+        editorProps: {
+            attributes: {
+                class: 'prose prose-sm sm:prose-base lg:prose-lg m-5 focus:outline-none'
+            }
+        }
+
+    })
+    if (!editor) {
+        return null
+    }
+    async function publishBlog() {
+        if (!editor) return 
+        const blogJSON = editor.getJSON();
+        const res = await axios.post(`${BACKEND_URL}/api/v1/blog`, blogJSON, {
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        })
+    }
     return <div>
-        <AppBar type="editor" /> 
+        <AppBar type="editor" onClick={publishBlog} />
         <div className="flex justify-center">
             <div className="flex justify-center mt-15 max-w-4xl">
-                <Tiptap />
+                <Tiptap editor={editor}/>
             </div>
         </div>
     </div>
